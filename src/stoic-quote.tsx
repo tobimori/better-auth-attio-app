aaceb907b4c2799cc647a6a09705621aecd5072c
tobimori <tobias@moeritz.io>
2025-08-15 20:51:39 +0200

    remove effect for runtime incompatibilities

--- src/stoic-quote.tsx
+++ src/stoic-quote.tsx
@@ -3,19 +3,18 @@ import {Column, TextBlock, useAsyncCache} from "attio/client"
 import getStoicQuote from "./get-stoic-quote.server"
 
 export function StoicQuote({recordId}: {recordId: string}) {
-    // By passing in the recordId, the result will be cached for each recordId
-    const {
-        values: {quote},
-        //       ^^^^^– this key matches
-        //             vvvvv– this key
-    } = useAsyncCache({quote: [getStoicQuote, recordId]})
-    //                         ^^^^^^^^^^^^^  ^^^^^^^^
-    //                         async fn       parameter(s)
+  // By passing in the recordId, the result will be cached for each recordId
+  const {
+    values: {quote},
+    //       ^^^^^– this key matches
+    //             vvvvv– this key
+  } = useAsyncCache({quote: [getStoicQuote, recordId]})
+  //                         ^^^^^^^^^^^^^  ^^^^^^^^
+  //                         async fn       parameter(s)
 
-    return (
-        <Column>
-            <TextBlock align="center">{quote.text}</TextBlock>
-            <TextBlock align="right">– {quote.author}</TextBlock>
-        </Column>
-    )
+  return (
+    <Column>
+      <TextBlock align="center">{JSON.stringify(quote)}</TextBlock>
+    </Column>
+  )
 }
